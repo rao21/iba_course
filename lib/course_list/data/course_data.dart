6ef7103042ace95dbcbafd3e7e55d707687c306c
rao21 <rao.noman786@outlook.com>
2023-02-06 09:19:48 +0500

    Data model added.

---
+++ lib/course_list/data/course_data.dart
@@ -0,0 +1,6 @@
+class CourseModel {
+  final String name;
+  final String code;
+  final String tech;
+  CourseModel({required this.name, required this.code, required this.tech});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iba_course/course_list/view/course_list.dart';
 import 'package:iba_course/login.dart';
 
 void main() {
@@ -14,12 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(
-          body: MobileLoginForm(
-              )),
+      home: const Scaffold(body: CourseList()),
     );
   }
 }
-
-
-
