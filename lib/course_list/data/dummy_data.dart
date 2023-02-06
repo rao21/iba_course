7b20cd18c6bb74596d9c08b734d8dede8499fe73
rao21 <rao.noman786@outlook.com>
2023-02-06 09:19:55 +0500

    Dummy data added.

---
+++ lib/course_list/data/dummy_data.dart
@@ -0,0 +1,6 @@
+import 'package:iba_course/course_list/data/course_data.dart';
+
+List<CourseModel> data = [
+  CourseModel(code: '1234', name: 'App craft', tech: 'Flutter'),
+  CourseModel(code: '1234', name: 'App craft', tech: 'Flutter')
+];
