8353df7f73325adb617f996c2dac3ff028c1d9fc
rao21 <rao.noman786@outlook.com>
2023-03-15 09:12:26 +0500

    Provider basic.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:iba_course/mid/api.dart';
+import 'package:iba_course/provider/ui.dart';
 import 'package:iba_course/widget/local_widget.dart';
+import 'package:provider/provider.dart';
 
 import 'api_data/view.dart';
 
@@ -13,14 +15,27 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(body: MakeUpProducts()),
-    );
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => Counter()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Scaffold(body: ProviderCounter()),
+      ));
+    // return MaterialApp(
+    //   title: 'Flutter Demo',
+    //   localizationsDelegates: AppLocalizations.localizationsDelegates,
+    //   supportedLocales: AppLocalizations.supportedLocales,
+    //   theme: ThemeData(
+    //     primarySwatch: Colors.blue,
+    //   ),
+    //   home: const Scaffold(body: MakeUpProducts()),
+    // );
   }
 }
