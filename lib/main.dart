1703313d8452d398c56fddc48d5bd161e65e88f2
rao21 <rao.noman786@outlook.com>
2023-03-20 10:35:54 +0500

    Add bloc on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:iba_course/mid/api.dart';
-import 'package:iba_course/provider/ui.dart';
-import 'package:iba_course/widget/local_widget.dart';
-import 'package:provider/provider.dart';
-
-import 'api_data/view.dart';
+import 'package:iba_course/bloc/ui.dart';
+// import 'package:iba_course/provider/ui.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,27 +11,14 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => Counter()),
-      ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: AppLocalizations.supportedLocales,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const Scaffold(body: ProviderCounter()),
-      ));
-    // return MaterialApp(
-    //   title: 'Flutter Demo',
-    //   localizationsDelegates: AppLocalizations.localizationsDelegates,
-    //   supportedLocales: AppLocalizations.supportedLocales,
-    //   theme: ThemeData(
-    //     primarySwatch: Colors.blue,
-    //   ),
-    //   home: const Scaffold(body: MakeUpProducts()),
-    // );
+    return MaterialApp(
+      title: 'Flutter Demo',
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(body: HomePage()),
+    );
   }
 }
