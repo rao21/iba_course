0afe49fb17794c460b79920295ba0feaf2bfd66b
rao21 <rao.noman786@outlook.com>
2023-03-20 10:34:49 +0500

    Added user repo

---
+++ lib/bloc/repo/user_repo.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:iba_course/bloc/model/user_model.dart';
+
+class UserRepository {
+  String userUrl = 'https://reqres.in/api/users?page=1';
+
+  Future<List<UserModel>> getUsers() async {
+    Response response = await get(Uri.parse(userUrl));
+
+    if (response.statusCode == 200) {
+      final List result = jsonDecode(response.body)['data'];
+      return result.map((e) => UserModel.fromJson(e)).toList();
+    } else {
+      throw Exception(response.reasonPhrase);
+    }
+  }
+}
